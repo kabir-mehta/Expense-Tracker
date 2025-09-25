d96a616c1c50815c84a34ef95ac4ed5518838577
Kabir Mehta <151664386+kabir-mehta@users.noreply.github.com>
2025-09-25 11:11:41 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
   const [username, setUsername] = useState("");
